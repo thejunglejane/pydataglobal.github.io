8c70b55bd2d22682612ec194a26a13c105108694
DiegoAlbertoTorres <DiegoAlbertoTorres@users.noreply.github.com>
2020-11-09 07:37:10 +0000

    Deploy to GitHub pages

---
+++ default.nix
@@ -0,0 +1,15 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "global.pydata.org";
+  buildInputs = [
+    jekyll
+    ruby_2_7
+    zlib
+    libffi
+  ];
+  src = null;
+  shellHook = ''
+    export GEM_HOME="$PWD/gems"
+  '';
+}
